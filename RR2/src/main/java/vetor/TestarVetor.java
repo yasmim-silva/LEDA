201ec3a4df901297d89e9f98209c9b23aae45801
123210278 YASMIM DA SILVA RODRIGUES <yasmim.silva.rodrigues@lcc2-53.lcc.ufcg.edu.br>
2024-12-04 09:45:56 -0300

    envio teste

--- RR2/src/main/java/vetor/TestarVetor.java
+++ RR2/src/main/java/vetor/TestarVetor.java
@@ -6,5 +6,16 @@ public class TestarVetor {
 		// preencha esse metodo com codigo para testar a classe vetor.
 		// À medida que voce evoluir no exercício o conteúdo deste mpetodo
 		// também será modificado.
+
+		Aluno aluno1 = new Aluno("lucas", 7.0);
+		Aluno aluno2 = new Aluno("alice", 7.5);
+		Aluno aluno3 = new Aluno("maria", 5.0);
+
+		Vetor v = new Vetor(3);
+		v.inserir(aluno1);
+		v.inserir(aluno2);
+		v.inserir(aluno3);
+
+		//System.out.println(v.);
 	}
 }
