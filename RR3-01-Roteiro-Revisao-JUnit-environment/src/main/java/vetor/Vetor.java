760f6d45a4695106b0eeb7e73d60c0eb92112e0b
123210278 YASMIM DA SILVA RODRIGUES <yasmim.silva.rodrigues@lcc2-53.lcc.ufcg.edu.br>
2024-12-06 11:23:39 -0300

    envio RR4

---
+++ RR3-01-Roteiro-Revisao-JUnit-environment/src/main/java/vetor/Vetor.java
@@ -0,0 +1,134 @@
+package vetor;
+
+import java.util.Comparator;
+
+/**
+ * Implementação de um vetor de objetos simples para exercitar os conceitos de
+ * Generics.
+ * 
+ * @author Adalberto
+ *
+ */
+public class Vetor<T extends Comparable<T>>{
+
+	// O array interno onde os objetos manipulados são guardados
+	private T[] arrayInterno;
+
+	// O tamanho que o array interno terá
+	private int tamanho;
+
+	// Indice que guarda a proxima posição vazia do array interno
+	private int indice;
+
+	// O Comparators a serem utilizados
+	private Comparator comparadorMaximo;
+	private Comparator comparadorMinimo;
+
+	@SuppressWarnings("unchecked")
+	public Vetor(int tamanho) {
+		super();
+		this.tamanho = tamanho;
+		this.indice = -1;
+		this.arrayInterno = (T[]) new Comparable[tamanho];
+	}
+
+	private int procurarIndice(T o) {
+		for (int i = 0; i < this.arrayInterno.length; i++) {
+			if (this.arrayInterno[i] != null && this.arrayInterno[i] == o) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public void setComparadorMaximo(Comparator comparadorMaximo) {
+		this.comparadorMaximo = comparadorMaximo;
+	}
+
+	public void setComparadorMinimo(Comparator comparadorMinimo) {
+		this.comparadorMinimo = comparadorMinimo;
+	}
+
+	// Insere um objeto no vetor
+	public void inserir(T o) {
+		this.indice++;
+		this.arrayInterno[this.indice] = o;
+	}
+
+	// Remove um objeto do vetor
+	public T remover(T o) {
+		int indice = procurarIndice(o);
+		if (indice == -1) {
+			throw new RuntimeException("Objeto não encontrado no vetor.");
+		}
+		for (int i = indice; i < this.tamanho-1; i++) {
+			this.arrayInterno[i] = this.arrayInterno[i+1];
+		}
+		this.arrayInterno[tamanho-1] = null;
+		this.indice--;
+		return o;
+	}
+
+	// Procura um elemento no vetor
+	public T procurar(T o) {
+		int indice = procurarIndice(o);
+		if (indice == -1) {
+			throw new RuntimeException("Objeto não encontrado no vetor.");
+		} 
+		return this.arrayInterno[indice];
+	}
+
+	// Diz se o vetor está vazio
+	public boolean isVazio() {
+		return this.indice == -1;
+	}
+
+	// Diz se o vetor está cheio
+	public boolean isCheio() {
+		return this.indice == this.tamanho-1;
+	}
+
+	public T vetorMaximo() {
+		if (isVazio()) {
+			return null;
+		}
+		T max = this.arrayInterno[0];
+		for (int i = 1; i < this.arrayInterno.length; i++) {
+			if (this.comparadorMaximo.compare(max, this.arrayInterno[i]) < 0) {
+				max = this.arrayInterno[i];
+			}
+		}
+		return max;
+	}
+
+	public T vetorMinimo() {
+		if (isVazio()) {
+			return null;
+		}
+		T min = this.arrayInterno[0];
+		for (int i = 1; i < this.arrayInterno.length; i++) {
+			if (this.comparadorMinimo.compare(min, this.arrayInterno[i]) > 0) {
+				min = this.arrayInterno[i];
+			}
+		}
+		return min;
+	}
+}
+
+class ComparadorMaximo<E extends Comparable<E>> implements Comparator<E> {
+
+	@Override
+	public int compare(E o1, E o2) {
+		return o1.compareTo(o2);
+	}
+
+}
+
+class ComparadorMinimo<E extends Comparable<E>> implements Comparator<E> {
+
+	@Override
+	public int compare(E o1, E o2) {
+		return o2.compareTo(o1);
+	}
+
+}
---
+++ RR3-01-Roteiro-Revisao-JUnit-environment/src/test/java/vetor/TestVetor.java
@@ -0,0 +1,51 @@
+package vetor;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class TestVetor {
+
+    @Test
+	public void testInserir() {
+		Aluno aluno1 = new Aluno("lucas", 7.0);
+		Aluno aluno2 = new Aluno("alice", 7.5);
+
+		Vetor<Aluno> v = new Vetor<Aluno>(3);
+		v.inserir(aluno1);
+		v.inserir(aluno2);
+		assertFalse(v.isVazio());
+	}
+
+	@Test
+	public void testRemover() {
+		Aluno aluno1 = new Aluno("lucas", 7.0);
+
+		Vetor<Aluno> v = new Vetor<Aluno>(1);
+		v.inserir(aluno1);
+		assertFalse(v.isVazio());
+
+		v.remover(aluno1);
+		assertTrue(v.isVazio());
+	}
+
+	@Test
+	public void testProcurar() {
+		Aluno aluno1 = new Aluno("lucas", 7.0);
+
+		Vetor<Aluno> v = new Vetor<Aluno>(1);
+		v.inserir(aluno1);
+
+		assertEquals(aluno1, v.procurar(aluno1));
+	}
+
+	@Test
+	public void testVetorMaximo() {
+		Aluno aluno1 = new Aluno("lucas", 7.0);
+		Aluno aluno2 = new Aluno("alice", 7.5);
+
+		Vetor<Aluno> v = new Vetor<Aluno>(3);
+		v.inserir(aluno1);
+		v.inserir(aluno2);
+	}
+}
