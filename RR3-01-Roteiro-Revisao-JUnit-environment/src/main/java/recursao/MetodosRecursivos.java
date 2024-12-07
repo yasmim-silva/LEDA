726375640f73ef9194499363cdbb016f744e05e1
yasmim-silva <yasmim.silva.rodrigues@ccc.ufcg.edu.br>
2024-12-07 15:15:24 -0300

    finalizado

--- RR3-01-Roteiro-Revisao-JUnit-environment/src/main/java/recursao/MetodosRecursivos.java
+++ RR3-01-Roteiro-Revisao-JUnit-environment/src/main/java/recursao/MetodosRecursivos.java
@@ -1,69 +1,80 @@
 package recursao;
 
+import java.util.Arrays;
+
 public class MetodosRecursivos {
 
 	public int calcularSomaArray(int[] array){
-		int result = 0;
-		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR A SOMA
-		// DOS EMENTOS DE UM ARRAY
-		return result;
+		if (array.length == 0) {
+			return 0;
+		}
+		return array[0] + calcularSomaArray(Arrays.copyOfRange(array, 1, array.length));
 	}
 	public long calcularFatorial(int n) {
-		long result = 1;
-		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR E IMPRIMIR
-		// O FATORIAL DO PARAMETRO
-		// DE ACORDO COM O QUE FOI MOSTRADO NO EXERCCICIO. OBSERVE QUE SENDO O
-		// METODO
-		// RECURSIVO, O FATORIAL DOS NUMEROS ANTERIORES TAMBEM VAO SER IMPRESSOS
-		return result;
+		if (n == 0) {
+			System.out.println(n + "! = 1");
+			return 1;
+		} else {
+			long result = n * (calcularFatorial(n - 1));
+			System.out.println(n + "! = " + result);
+			return result;
+		}
 	}
 
 	public int calcularFibonacci(int n) {
-		int result = 1;
-		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR E IMPRIMIR
-		// O N-ESIMO TERMO
-		// DA SEQUENCIA DE FIBONACCI, QUE TEM A SEGUINTE LEI DE FORMACAO: O
-		// PRIMEIRO E SEGUNDO NUMEROS
-		// DA SEQUENCIA SÃO 1. A PARTIR DO TERCEIRO TERMO, CADA TERMO DA
-		// SEQUENCIA É DADO
-		// PELA SOMA DOS OUTROS DOIS ANTERIORES. OBSERVE QUE SENDO O METODO
-		// RECURSIVO, O FIBONACCI DOS NUMEROS ANTERIORES TAMBEM VAO SER
-		// IMPRESSOS
-		return result;
+		if (n == 0) {
+			return 1;
+		} 
+		if (n == 1) {
+			return 1;
+		} else {
+			int result = calcularFibonacci(n - 1) + calcularFibonacci(n-2);
+			return result;
+		}
 	}
 
 	public int countNotNull(Object[] array) {
 		int result = 0;
-		// TODO IMPLEMENTE AQUI O CODIGO QUE CONTA (USANDO RECURSAO) A
-		// QUANTIDADE DE ELEMENTOS NAO NULOS
-		// DE UM ARRAY DE OBJETOS RECEBIDO COMO PARAMETRO
+		int indice = 0;
+		if (array[indice] != null) {
+			result += 1;
+		}
+		if ((indice + 1) != array.length) {
+			indice++;
+			Object[] novoArray = Arrays.copyOfRange(array, indice, array.length);
+			result += countNotNull(novoArray);
+		}
 		return result;
 	}
 
 	public long potenciaDe2(int expoente) {
-		int result = 1;
-		// TODO IMPLEMENTE (USANDO RECURSAO) O CODIGO PARA PRODUZIR A N-ESIMA
-		// POTENCIA
-		// DE 2
+		long result = 0;
+		if (expoente == 0) {
+			return 1;
+		}
+		if (expoente == 1) {
+			return 2;
+		} 
+		if (expoente - 1 != 0) {
+			result += 2 * potenciaDe2(expoente-1);
+		}
 		return result;
 	}
 
 	public double progressaoAritmetica(double termoInicial, double razao, int n) {
-		double result = 0;
-		// TODO IMPLEMENTE SEU CODIGO (USANDO RECURSAO) DE ENCONTRAR O n-ESIMO
-		// TERMO
-		// DA PROGRESSAO ARITMETICA, DADO O TERMO INICIAL E A RAZAO
-		// VOCE NAO PODE USAR A FORMULA QUE CALCULA O N-ESIMO TERMO. DEVE USAR RECURSAO
-		return result;
+		if (n == 1) {
+			return termoInicial;
+		} else {
+			return razao + progressaoAritmetica(termoInicial, razao, n - 1);
+		}
 	}
 
 	public double progressaoGeometrica(double termoInicial, double razao, int n) {
-		double result = 1;
-		// TODO IMPLEMENTE SEU CODIGO (USANDO RECURSAO) DE ENCONTRAR O n-ESIMO
-		// TERMO
-		// DA PROGRESSAO GEOMETRICA, DADO O TERMO INICIAL E A RAZAO
-		// VOCE NAO PODE USAR A FORMULA QUE CALCULA O N-ESIMO TERMO. DEVE USAR RECURSAO
-		return result;
+		if (n == 1) {
+			return termoInicial;
+		} else {
+			return razao * progressaoGeometrica(termoInicial, razao, n - 1);
+		}
 	}
 	
 	
