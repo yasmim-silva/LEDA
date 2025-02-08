3d82deae2477d3aa6a7e11f0ac6bf27da5e5a783
yasmim-silva <yasmim.silva.rodrigues@ccc.ufcg.edu.br>
2025-02-08 10:35:54 -0300

    R04 finalizado

--- R04-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
+++ R04-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -17,22 +17,59 @@ public class CountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		Integer maior = leftIndex;
-		//private void calculaMaior(array, leftIndex) {
-			for (int i = leftIndex + 1; i < array.length; i++) {
-				if (i > maior) {
-					maior = i;
-				}
-			}
-		//}
-		//Criando um novo array do tamanho do maior elemento.
-		Integer[] A = new Integer[maior];
-		for (int i = leftIndex; i < array.length; i++) {
-			A[array[i] - 1] += 1;
-		}
-		for (int i = 1; i < A.length; i++) {
-			A[i] = A[i] + A[i-1];
-		}
+		if (array == null || leftIndex < 0 || rightIndex >= array.length || leftIndex >= rightIndex) {
+            return;
+        }
+		ordenaArray(array, leftIndex, rightIndex);
 	}
 
+	private int calculaMaior(Integer[] array, int leftIndex, int rightIndex) {
+		int maior = array[leftIndex];
+
+		//Obtém o valor do maior elemento do array.
+		for (int i = leftIndex + 1; i <= rightIndex; i++) {
+			if (array[i] > maior) {
+				maior = array[i]; 
+			}
+		}
+
+		return maior;
+	}
+
+	private Integer[] frequenciaAcumulada(Integer[] array, int leftIndex, int rightIndex) {
+		Integer[] A = new Integer[calculaMaior(array, leftIndex, rightIndex)+1];
+		for (int i = 0; i < A.length; i++) {
+            A[i] = 0;
+        }
+
+		//Calcula o número de ocorrências dos elementos do array.
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			A[array[i]] += 1; 
+		}
+
+		//Acumula as ocorrências.
+		for (int i = 1; i < A.length; i++) {
+			A[i] = A[i] + A[i-1]; 
+		}
+
+		return A;
+	}
+
+	private Integer[] ordenaArray(Integer[] array, int leftIndex, int rightIndex) {
+		Integer[] B = new Integer[rightIndex - leftIndex + 1];
+		Integer[] frequencia = frequenciaAcumulada(array, leftIndex, rightIndex);
+
+		for (int i = rightIndex; i >= leftIndex; i--) {
+			int indice = frequencia[array[i]] - 1;
+			B[indice] = array[i];
+			frequencia[array[i]]--; //Atualiza a posição.
+		}
+
+		//Copia os elementos do array B para o array original.
+		for (int i = 0; i < B.length; i++) {
+            array[leftIndex + i] = B[i]; 
+        }
+
+		return array;
+	}
 }
--- R04-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R04-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -12,8 +12,68 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (array == null || leftIndex < 0 || rightIndex >= array.length || leftIndex >= rightIndex) {
+            return;
+        }
+		ordenaArray(array, leftIndex, rightIndex);	
 	}
 
+	private int[] calculaExtremos(Integer[] array, int leftIndex, int rightIndex) {
+		int maior = array[rightIndex];
+		int menor = array[leftIndex];
+
+		for (int i = leftIndex + 1; i < rightIndex; i++) {
+			if (array[i] > maior) {
+				maior = array[i]; 
+			}
+			if (array[i] < menor) {
+				menor = array[i];
+			}
+		}
+
+		return new int[]{menor, maior};
+	}
+
+	private Integer[] frequenciaAcumulada(Integer[] array, int leftIndex, int rightIndex) {
+		int[] extremos = calculaExtremos(array, leftIndex, rightIndex);
+    	int menor = extremos[0];
+    	int maior = extremos[1];
+		Integer[] A = new Integer[maior - menor + 1];
+
+		for (int i = 0; i < A.length; i++) {
+            A[i] = 0;
+        }
+
+		//Calcula o número de ocorrências dos elementos do array.
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			A[array[i] - menor]++;
+		}
+
+		//Acumula as ocorrências.
+		for (int i = 1; i < A.length; i++) {
+			A[i] = A[i] + A[i-1]; 
+		}
+
+		return A;
+	}
+
+	private Integer[] ordenaArray(Integer[] array, int leftIndex, int rightIndex) {
+		Integer[] B = new Integer[rightIndex - leftIndex + 1];
+		Integer[] frequencia = frequenciaAcumulada(array, leftIndex, rightIndex);
+		int[] extremos = calculaExtremos(array, leftIndex, rightIndex);
+    	int menor = extremos[0];
+
+		for (int i = rightIndex; i >= leftIndex; i--) {
+			int indice = frequencia[array[i] - menor] - 1;
+			B[indice] = array[i];
+			frequencia[array[i] - menor]--; //Atualiza a posição.
+		}
+
+		//Copia os elementos do array B para o array original.
+		for (int i = 0; i < B.length; i++) {
+            array[leftIndex + i] = B[i]; 
+        }
+
+		return array;
+	}
 }
